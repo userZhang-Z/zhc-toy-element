6d7281e07ac13f67db4f24ce1866ee3e5530642a
userZhang-Z <3065060034@qq.com>
2025-01-03 14:52:18 +0800

    :tada：first commit

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+/**
+ * Button.vue
+ * Button.test.tsx
+ * types.ts
+ * style.css
+ * components
+ */
+
+defineOptions({
+    name:'ErButton'
+})
+</script>
+
+<template>
+    <button style="background-color: blue;color: red;">
+        this is a button
+    </button>
+    
+</template>
---
+++ packages/components/Button/index.ts
@@ -0,0 +1,6 @@
+
+import Button from "./Button.vue";
+
+import { WithInstall } from "@toy-element/utils";
+
+export const ErButton= WithInstall(Button);
